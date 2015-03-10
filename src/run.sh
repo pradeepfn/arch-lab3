c7c3da08a81166e4116f448135dcc6e5e9fd66b5
Pradeep Fernando <pradeepfn@gmail.com>
2015-03-10 14:45:01 -0400

    script modifications

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
-#./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100000 -i ../new_traces/100k.bzip.gz > bzip.output
-#./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100000 -i ../new_traces/100k.gcc.gz > gcc.output
-#./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100000 -i ../new_traces/100k.libq.gz > libq.output
-#./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100000 -i ../new_traces/100k.mcf.gz > mcf.output
+./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100000 -i ../new_traces/100k.bzip.gz > bzip.output
+./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100000 -i ../new_traces/100k.gcc.gz > gcc.output
+./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100000 -i ../new_traces/100k.libq.gz > libq.output
+./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100000 -i ../new_traces/100k.mcf.gz > mcf.output
 ./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100 -i ../new_traces/sml.ptr.gz > sml.output
---
+++ src/sml.sh
@@ -0,0 +1,1 @@
+./procsim -r 3 -f 4 -j 2 -k 1 -l 2 -b 1 -e 100 -i ../new_traces/sml.ptr.gz
